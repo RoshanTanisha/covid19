28cc499df2f360dc7f000fc75e5aefc866e087b0
RoshanTanisha <t.bhayani@yahoo.com>
2020-08-13 19:42:13 +0530

    updated main.py file

---
+++ code/main.py
@@ -0,0 +1,29 @@
+import h5py
+import numpy as np
+
+from code.model import UNetClassifier
+
+
+def load_dataset(covid_file_path, normal_file_path):
+    covid = h5py.File(covid_file_path, 'r')['covid']
+    normal = h5py.File(normal_file_path, 'r')['normal']
+
+    all_images = np.expand_dims(np.concatenate([covid, normal]), axis=3)
+    all_labels = np.concatenate([[1]*covid.shape[0], [0]*normal.shape[0]])
+
+    shuffled_indices = np.random.permutation(np.arange(all_images.shape[0]))
+
+    all_images = all_images[shuffled_indices]
+    all_labels = all_labels[shuffled_indices]
+
+    return all_images, all_labels
+
+if __name__ == '__main__':
+    model = UNetClassifier((512, 512, 1), 2, True)
+        
+    all_images, all_labels = load_dataset()
+
+    print(all_images.shape, all_labels.shape)
+
+    model.train(all_images, all_labels, 15, 16, 0.2)
+
